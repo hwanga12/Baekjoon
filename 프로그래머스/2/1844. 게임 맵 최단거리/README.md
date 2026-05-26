6ee171c955f3f5692d04326228f2623e294ae496
황가연 <ghkdrkdus1@naver.com>
2026-05-26 13:46:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.54 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 9.25 MB, 시간: 7.11 ms
+메모리: 9.26 MB, 시간: 7.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 26일 13:06:15
+2026년 05월 26일 13:46:28
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,41 +1,33 @@
 from collections import deque
 
 def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
     
-    # 4방향 (상, 하, 좌, 우)
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    
-    # BFS를 위한 큐 생성 및 시작점(0, 0) 삽입
     queue = deque()
     queue.append((0, 0))
     
-    # 큐가 빌 때까지 반복
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
+    n = len(maps)
+    m = len(maps[0])
+    
+    
     while queue:
-        x, y = queue.popleft()
-        
-        # 현재 위치에서 4방향으로 물결 퍼뜨리기
+        y, x = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            # 맵을 벗어났거나, 벽(0)을 만나면 무시
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            if nx < 0 or ny < 0 or nx >= m or ny >= n:
                 continue
-            if maps[nx][ny] == 0:
+            if maps[ny][nx] == 0:
                 continue
+            
+            if maps[ny][nx] == 1:
+                maps[ny][nx] = maps[y][x]+ 1
+                queue.append((ny, nx))
                 
-            # 아직 안 가본 길(1)이라면
-            if maps[nx][ny] == 1:
-                # ★ 핵심: 이전 칸까지 온 거리에 + 1을 해서 내 칸에 적어둔다!
-                maps[nx][ny] = maps[x][y] + 1
-                queue.append((nx, ny))
-                
-    # 도착점(우측 하단)의 값이 1보다 크면 도달 성공 (최단 거리)
     if maps[n-1][m-1] > 1:
         return maps[n-1][m-1]
-    # 도착점이 여전히 1이라면(또는 벽에 막혀서 못 갔다면) 도달 실패
-    else:
+    elif maps[n-1][m-1] == 1:
         return -1
