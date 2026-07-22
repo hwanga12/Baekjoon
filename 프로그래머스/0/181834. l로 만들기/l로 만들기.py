d1c4d9a512cf68fb28cbe5c27752824e779d2bea
황가연 <ghkdrkdus1@naver.com>
2026-07-22 23:52:28 +0900

    [level 0] Title: l로 만들기, Time: 8.51 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,10 @@
+def solution(myString):
+    answer = ''
+
+    for i in range(len(myString)):
+        if myString[i] < 'l':
+            answer += 'l'
+        else:
+            answer += myString[i]
+
+    return answer
