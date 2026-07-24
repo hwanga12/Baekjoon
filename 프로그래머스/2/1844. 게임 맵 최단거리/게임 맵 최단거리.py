aef2b45bf46bf66315fc9650bb385fd13deca3b3
황가연 <ghkdrkdus1@naver.com>
2026-07-25 00:45:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.97 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,33 +1,33 @@
 from collections import deque
 
 def solution(maps):
-    
-    queue = deque()
-    queue.append((0, 0))
-    
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    
-    n = len(maps)
-    m = len(maps[0])
-    
-    
+    rows = len(maps)
+    cols = len(maps[0])
+
+    queue = deque([(0, 0)])
+    visited = [[False] * cols for _ in range(rows)]
+    visited[0][0] = True
+
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
     while queue:
-        y, x = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            
-            if nx < 0 or ny < 0 or nx >= m or ny >= n:
-                continue
-            if maps[ny][nx] == 0:
-                continue
-            
-            if maps[ny][nx] == 1:
-                maps[ny][nx] = maps[y][x]+ 1
-                queue.append((ny, nx))
-                
-    if maps[n-1][m-1] > 1:
-        return maps[n-1][m-1]
-    elif maps[n-1][m-1] == 1:
-        return -1
+        x, y = queue.popleft()
+
+        if x == rows - 1 and y == cols - 1:
+            return maps[x][y]
+
+        for dx, dy in directions:
+            nx = x + dx
+            ny = y + dy
+
+            if (
+                0 <= nx < rows
+                and 0 <= ny < cols
+                and maps[nx][ny] == 1
+                and not visited[nx][ny]
+            ):
+                visited[nx][ny] = True
+                maps[nx][ny] = maps[x][y] + 1
+                queue.append((nx, ny))
+
+    return -1
